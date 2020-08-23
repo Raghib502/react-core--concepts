31f1be79be37671006bec880135d91a20e835427
Raghib Ahsan <40718368+Raghib502@users.noreply.github.com>
2020-08-23 16:40:45 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My first react app </p>
+        <Person name="Shakib khan" home="DHK"></Person>
+        <Person name ="Rubel" home="CTG"></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(props){
+  use
+  const personStyle={
+    border : '2px solid red',
+    margin: '10px',
+    width : '400px'
+  }
+  return (
+  <div style={personStyle}>
+    <h1>Name:{props.name}</h1>
+  <h3>Address:{props.home}</h3>
+  </div>
+  )
+    
 }
 
 export default App;
